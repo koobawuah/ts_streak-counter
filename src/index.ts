079ea0ad71d9b5ea82fc020df558022f39815284
koobawuah <me@bawuahboakye.com>
2022-06-10 12:30:29 +0000

    Updating the functionality and tests of the TypeScript streak counter application library

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,57 @@
-import { formattedDate } from './utils'
-
-interface Streak {
-    currentCount: number,
-    startDate: string,
-    lastLoginDate: string,
-}
+import { Streak, formattedDate, buildStreak, updateStreak } from './utils'
 
 const KEY = 'streak'
 
-//come back and complete the streakCounter function for the application
+function shouldIncrementOrResetStreakCount( currentDate:Date, lastLoginDate:string,): 'increment'| 'reset' | 'none' {
+
+    const difference = currentDate.getDate() - parseInt(lastLoginDate.split('/')[1])
+
+    if (difference === 0) {
+        return 'none'
+    }
+    if (difference === 1) {
+        return 'increment'
+    }
+
+    return 'reset'
+}
+
+
 export function streakCounter(storage: Storage, date: Date):Streak {
 
     const streakInLocalStorage = storage.getItem(KEY);
     if(streakInLocalStorage) {
         try{
-            const streak = JSON.parse(streakInLocalStorage || "")
+            const streak = JSON.parse(streakInLocalStorage) as Streak;
+            const state = shouldIncrementOrResetStreakCount(date, streak.lastLoginDate);
+            const SHOULD_INCREMENT = state === "increment";
+            const SHOULD_RESET = state === "reset";
+
+            if (SHOULD_INCREMENT) {
+                const updatedStreak: Streak = buildStreak(date, {
+                    startDate: streak.startDate,
+                    currentCount: streak.currentCount + 1,
+                    lastLoginDate: formattedDate(date),
+                })
+
+                // store in localStorage
+                updateStreak(storage, updatedStreak);
+
+                return updatedStreak
+            }
+            if (SHOULD_RESET) {
+                const updatedStreak = buildStreak(date) 
+                storage.setItem(KEY, JSON.stringify(updatedStreak))
+
+                return updatedStreak
+            }
             return streak
         } catch(error) {
             console.error("Failed to parse streak from localStorage")
         }
     }
 
-    const streak =  {
-        currentCount: 1,
-        startDate: formattedDate(date),
-        lastLoginDate: formattedDate(date),
-    }
+    const streak = buildStreak(date)
 
     storage.setItem(KEY, JSON.stringify(streak))
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,30 @@
 
+export const KEY = 'streak'
+
+export interface Streak {
+    currentCount: number,
+    startDate: string,
+    lastLoginDate: string,
+}
 
 export function formattedDate(date: Date): string {
 
-            return date.toLocaleDateString('en-US')
-        }
+    return date.toLocaleDateString('en-US')
+}
+
+export function updateStreak(storage: Storage, streak: Streak): void {
+    storage.setItem(KEY, JSON.stringify(streak))
+}
+
+export function buildStreak(date: Date, overrideDefaults?: Partial<Streak>, ):Streak {
+    const defaultStreak = {
+        currentCount: 1,
+        startDate: formattedDate(date),
+        lastLoginDate: formattedDate(date)
+    } 
+
+    return {
+        ...defaultStreak,
+        ...overrideDefaults,
+    }
+}
